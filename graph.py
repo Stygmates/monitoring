6123f1f47aa4cd56f3d4d130bdb6f5c6f6a2898d
Titan User <titan@titan.igbmc.u-strasbg.fr>
2017-08-28 16:48:51 +0200

    Ajout des seuils max, min et intermédiaires

--- graph.py
+++ graph.py
@@ -5,4 +5,8 @@ path = sys.argv[1]
 extension = '_sum-cor_gctf.log'
 filenames, phase_shifts = parser.get_phase_shifts(path, extension)
 plt.plot(phase_shifts, 'ro')
+plt.axhline(y = 180, color = 'k')
+plt.axhline(y = 35, color = 'k', linestyle = '--')
+plt.axhline(y = 145, color = 'k', linestyle = '--')
+plt.axhline(color = 'k')
 plt.show()
