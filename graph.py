697696efd54a4759c43e582bd4391168ebd068a2
tandat <styg.tan.dat@gmail.com>
2017-08-28 17:02:53 +0200

    Test changement de couleurs

--- graph.py
+++ graph.py
@@ -4,7 +4,14 @@ import matplotlib.pyplot as plt
 path = sys.argv[1]
 extension = '_sum-cor_gctf.log'
 filenames, phase_shifts = parser.get_phase_shifts(path, extension)
-plt.plot(phase_shifts, 'ro')
+for index, value in enumerate(phase_shifts):
+	if value > 180 or < 0:
+		plt.plot(index, value, 'ro')
+	elif value > 35 and < 145:
+		plt.plot(index, value, 'go')
+	else 
+		plt.plot(index, value, 'yo')
+
 plt.axhline(y = 180, color = 'k')
 plt.axhline(y = 35, color = 'k', linestyle = '--')
 plt.axhline(y = 145, color = 'k', linestyle = '--')
