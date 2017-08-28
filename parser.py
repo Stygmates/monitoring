8eeca03be08393fe96b115202b419d7346e8a36b
tandat <styg.tan.dat@gmail.com>
2017-08-28 14:16:11 +0200

    Ajout du graphe sur les phase shifts

--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+import os
 def get_stats(filename):
 	try:
 		file = open(filename,'r')
@@ -27,3 +28,44 @@ def get_stats(filename):
 
 		file.close()
 		return
+
+def get_phase_shift(filename):
+	try:
+		file = open(filename, 'r')
+	except IOError:
+		return
+	else:	
+		line = file.readline()
+		tab = []
+		while line !="":
+			line = file.readline()
+			tab.append(line)
+		tab.reverse()
+		for index, line in enumerate(tab):
+			if 'Final Values' in line:
+				file.close()
+				values = line.split()
+				parameters = tab[index + 1].split()
+				result = []
+				if 'Phase_shift' in parameters:
+					for i, parameter in enumerate(parameters):
+						if parameter == 'Phase_shift':
+							return values[i]
+				else:
+					return None
+
+
+def get_phase_shifts(path, extension):
+	logs = []
+	for file in os.listdir(path):
+		if file.endswith(extension):
+			logs.append(file)
+	logs.sort()
+	phase_shifts = []
+	filenames = []
+	for element in logs:
+		filename = path + element
+		if get_phase_shift(filename) is not None:
+			phase_shifts.append(get_phase_shift(filename))
+			filenames.append(element)
+	return filenames, phase_shifts
