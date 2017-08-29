290f5383a2fadea54db3a81730145b7d97c245ca
tandat <styg.tan.dat@gmail.com>
2017-08-29 14:55:02 +0200

    Suppression merge trace

--- graph.py
+++ graph.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 path = sys.argv[1]
 graph = sys.argv[2]
 extension = '_sum-cor_gctf.log'
-<<<<<<< HEAD
 if graph == 'phase_shift':
 	filenames, phase_shifts = parser.get_phase_shifts(path, extension)
 	plt.figure('Phase shifts graph')
@@ -20,12 +19,10 @@ elif graph == 'defocus':
 	plt.figure('Defocus graph')
 	if len(filenames) > 0:
 		plt.plot(defocus_u,defocus_v, 'ro')
-=======
 filenames, phase_shifts = parser.get_phase_shifts(path, extension)
 plt.plot(phase_shifts, 'ro')
 plt.axhline(y = 180, color = 'k')
 plt.axhline(y = 35, color = 'k', linestyle = '--')
 plt.axhline(y = 145, color = 'k', linestyle = '--')
 plt.axhline(color = 'k')
->>>>>>> b9db51bf1a0ab23788d1f0d615b72f9adbefcb01
 plt.show()
